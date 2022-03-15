ef8bc272678a3268b73970b4e4538a226d2c8c0a
Gloria Kyei <67934038+gigi-k93@users.noreply.github.com>
2022-03-15 11:41:09 +0200

    Add files via upload

---
+++ formhandler.php
@@ -0,0 +1,23 @@
+<?php
+$name= $_POST['name'];
+$visitor_email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+
+$email_from= 'gloriakyei93@gmail.com';
+$email_subject='New form submission';
+$email_body= "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject.\n".
+              "Message: $message.\n";
+
+$to= 'gloriakyei93@gmail.com';
+
+$headers= "From: $email_from\r\n";
+$headers= "Reply-To: $visitor_email\r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
+
+
+?>
